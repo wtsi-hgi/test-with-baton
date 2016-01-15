3909393d32f69eac062290ca4f5c08e963f728b6
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-01-15 13:30:27 +0000

    Improvement in helper tests.

--- testwithbaton/tests/test_helpers.py
+++ testwithbaton/tests/test_helpers.py
@@ -5,6 +5,9 @@ from hgicommon.collections import Metadata
 from testwithbaton.api import TestWithBatonSetup
 from testwithbaton.helpers import SetupHelper
 
+_TEST_METADATA = Metadata({"attribute_1": ["value_1", "value_2"], "attribute_2": ["value_3", "value_4"],
+                             "attribute_3": "value_5"})
+
 
 class TestSetupHelper(unittest.TestCase):
     """
@@ -45,30 +48,18 @@ class TestSetupHelper(unittest.TestCase):
     def test_add_metadata_to_data_object(self):
         path = self.setup_helper.create_data_object("filename")
 
-        metadata = Metadata({"attribute_1": ["value_1", "value_2"], "attribute_2": {"value_3", "value_4"},
-                             "attribute_3": "value_5"})
-        self.setup_helper.add_metadata_to(path, metadata)
+        self.setup_helper.add_metadata_to(path, _TEST_METADATA)
 
         retrieved_metadata = self.setup_helper.run_icommand("imeta", ["ls", "-d", path])
-        self.assertIn("attribute: attribute_1\nvalue: value_1", retrieved_metadata)
-        self.assertIn("attribute: attribute_1\nvalue: value_2", retrieved_metadata)
-        self.assertIn("attribute: attribute_2\nvalue: value_3", retrieved_metadata)
-        self.assertIn("attribute: attribute_2\nvalue: value_4", retrieved_metadata)
-        self.assertIn("attribute: attribute_3\nvalue: value_5", retrieved_metadata)
+        self._assert_metadata_in_retrieved(_TEST_METADATA, retrieved_metadata)
 
     def test_add_metadata_to_collection(self):
         path = self.setup_helper.create_collection("collection")
 
-        metadata = Metadata({"attribute_1": ["value_1", "value_2"], "attribute_2": {"value_3", "value_4"},
-                             "attribute_3": "value_5"})
-        self.setup_helper.add_metadata_to(path, metadata)
+        self.setup_helper.add_metadata_to(path, _TEST_METADATA)
 
         retrieved_metadata = self.setup_helper.run_icommand("imeta", ["ls", "-c", path])
-        self.assertIn("attribute: attribute_1\nvalue: value_1", retrieved_metadata)
-        self.assertIn("attribute: attribute_1\nvalue: value_2", retrieved_metadata)
-        self.assertIn("attribute: attribute_2\nvalue: value_3", retrieved_metadata)
-        self.assertIn("attribute: attribute_2\nvalue: value_4", retrieved_metadata)
-        self.assertIn("attribute: attribute_3\nvalue: value_5", retrieved_metadata)
+        self._assert_metadata_in_retrieved(_TEST_METADATA, retrieved_metadata)
 
     def test_get_checksum(self):
         file_name = "filename"
@@ -77,3 +68,18 @@ class TestSetupHelper(unittest.TestCase):
 
     def tearDown(self):
         self.test_with_baton.tear_down()
+
+    def _assert_metadata_in_retrieved(self, metadata: Metadata, retrieved_metadata: str):
+        """
+        Assert that the given metadata is in metadata retrieved via an `imeta` command.
+        :param metadata: the metadata to expect
+        :param retrieved_metadata: string representation of metadata, retrieved via an `imeta` command
+        """
+        for attribute in metadata:
+            attribute_values = metadata[attribute]
+            if not isinstance(attribute_values, list):
+                attribute_values = [attribute_values]
+
+            for value in attribute_values:
+                print(value)
+                self.assertIn("attribute: %s\nvalue: %s" % (attribute, value), retrieved_metadata)
