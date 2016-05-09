bca438c5cca8f17d7148a6dd14c5b9f94e3dba1b
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-05-09 17:01:54 +0100

    Added ability to specify the baton setup when ran through the CLI.

--- testwithbaton/main.py
+++ testwithbaton/main.py
@@ -1,16 +1,58 @@
+import argparse
 import json
 import logging
 import signal
 import sys
 
-from testwithbaton.api import TestWithBaton
+from testwithbaton.api import TestWithBaton, BatonSetup
+
+_PROGRAM_DESCRIPTION = "Sets up version of baton connected to an iRODS server for use in testing"
+_PROGRAM_BATON_VERSION_PARAMETER_NAME = "baton"
+_PROGRAM_BATON_VERSION_PARAMETER_HELP = "Version of baton to setup: [0.16.1, 0.16.2, 0.16.3]"
+_PROGRAM_IRODS_VERSION_PARAMETER_NAME = "irods"
+_PROGRAM_IRODS_VERSION_PARAMETER_HELP = "Version of iRODS to setup: [3.3.1, 4.1.8]"
+
+_DEFAULT_BATON_VERSION = "0.16.3"
+_DEFAULT_IRODS_VERSION = "3.3.1"
+
+
+def _parse_arguments() -> (str, str):
+    """
+    Parse the input arguments.
+    :return: tuple where the first item is the baton version and the second item is the iRODS version, both in the form
+    X.X.X
+    """
+    parser = argparse.ArgumentParser(description=_PROGRAM_DESCRIPTION)
+    parser.add_argument("--%s" % _PROGRAM_BATON_VERSION_PARAMETER_NAME, dest="baton_version",
+                        default=_DEFAULT_BATON_VERSION, help=_PROGRAM_BATON_VERSION_PARAMETER_HELP)
+    parser.add_argument("--%s" % _PROGRAM_IRODS_VERSION_PARAMETER_NAME, dest="irods_version",
+                        default=_DEFAULT_IRODS_VERSION, help=_PROGRAM_IRODS_VERSION_PARAMETER_HELP)
+
+    args = parser.parse_args()
+    return args.baton_version, args.irods_version
+
+
+def _get_baton_setup(baton_version: str, irods_version: str) -> BatonSetup:
+    """
+    Gets the baton setup for the given baton and iRODS versions.
+    :param baton_version: the baton version
+    :param irods_version: the iRODS version
+    :return: the baton setup
+    """
+    baton_setup_as_string = "v%s_WITH_IRODS_%s" % (baton_version.replace(".", "_"), irods_version.replace(".", "_"))
+    if baton_setup_as_string not in BatonSetup:
+        raise ValueError("No setup for baton version %s and iRODS version %s" % (baton_version, irods_version))
+    return BatonSetup.__dict__[baton_setup_as_string]
 
 
 def main():
     """
     Main method.
     """
-    test_with_baton = TestWithBaton()
+    baton_version, irods_version = _parse_arguments()
+    baton_setup = _get_baton_setup(baton_version, irods_version)
+
+    test_with_baton = TestWithBaton(baton_setup=baton_setup)
     test_with_baton.setup()
 
     setup_information = {
