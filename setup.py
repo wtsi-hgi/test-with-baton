0ee048a5716a03198e92908d941a3a62351cfe01
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-07-29 12:37:36 +0100

    Bumped to v1.1.0.

--- setup.py
+++ setup.py
@@ -2,23 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="test-with-baton",
-
-    version="1.0.0",
-
+    version="1.1.0",
     author="Colin Nolan",
-    author_email="hgi@sanger.ac.uk",
-
+    author_email="colin.nolan@sanger.ac.uk",
     packages=find_packages(exclude=["tests"]),
-
     url="https://github.com/wtsi-hgi/test-with-baton",
-
-    license="LICENSE",
-
+    license="LICENSE.txt",
     description="Simplifying the testing of software that depends on baton.",
     long_description=open("README.md").read(),
-
     install_requires=[x for x in open("requirements.txt").read().splitlines() if "://" not in x],
     dependency_links=[x for x in open("requirements.txt").read().splitlines() if "://" in x],
-
     test_suite="testwithbaton.tests"
 )
